2989920df81d4b598d42db4ba8063dd484422e88
ajh123 <samrosoft36@gmail.com>
2019-08-25 16:27:48 +0100

    Added some costs and started the Camera Class

--- CraftyEngineiers/Camera.h
+++ CraftyEngineiers/Camera.h
@@ -21,6 +21,19 @@
 
 enum Camera_Movement
 {
-    
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
 };
 
+const GLfloat YAW = -90.f;
+const GLfloat PITCH = 0.0f;
+const GLfloat SPEED = 6.0f;
+const GLfloat SENSEITIVITY = 0.25f;
+const GLfloat ZOOM = 45.0f;
+
+class Camera
+{
+    
+};
