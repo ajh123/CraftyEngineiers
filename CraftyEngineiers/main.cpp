9032e8b40a8ab43f797ac35f4b8c7e11cca0bead
ajh123 <samrosoft36@gmail.com>
2019-08-25 09:12:16 +0100

    Initial Commit

---
+++ CraftyEngineiers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CraftyEngineiers
+//
+//  Created by Samuel Hulme on 25/08/2019.
+//  Copyright © 2019 SJH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
